aa97060c4e421d412c13c58c8b1af7e0d99b32b0
Florian Liers <florian.liers@tu-ilmenau.de>
2013-08-22 09:29:26 +0200

    Added license (dual and EPL)
    Same licenses as in the fog.bus and fog.bus.view plug-ins.

--- fog.lowerlayer.view/src/de/tuilmenau/ics/fog/lowerlayer/view/PropertyAdapterFactory.java
+++ fog.lowerlayer.view/src/de/tuilmenau/ics/fog/lowerlayer/view/PropertyAdapterFactory.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Forwarding on Gates Simulator/Emulator - Lower Layer View
+ * Copyright (c) 2013, Integrated Communication Systems Group, TU Ilmenau.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html.
+ ******************************************************************************/
 package de.tuilmenau.ics.fog.lowerlayer.view;
 
 import org.eclipse.core.runtime.IAdapterFactory;
--- fog.lowerlayer/src/de/tuilmenau/ics/fog/lowerlayer/BindingImpl.java
+++ fog.lowerlayer/src/de/tuilmenau/ics/fog/lowerlayer/BindingImpl.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Forwarding on Gates Simulator/Emulator - Lower layer
+ * Copyright (C) 2013, Integrated Communication Systems Group, TU Ilmenau.
+ * 
+ * This program and the accompanying materials are dual-licensed under either
+ * the terms of the Eclipse Public License v1.0 as published by the Eclipse
+ * Foundation
+ *  
+ *   or (per the licensee's choosing)
+ *  
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ ******************************************************************************/
 package de.tuilmenau.ics.fog.lowerlayer;
 
 import net.rapi.Description;
